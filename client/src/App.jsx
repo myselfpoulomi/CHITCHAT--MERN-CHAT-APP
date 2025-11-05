31eee629b17f3e87911a0fb2ca36608d05d78b74
myselfpoulomi <routhpoulomi@gmail.com>
2025-11-06 02:14:36 +0530

    Add Tailwind CSS setup and update dependencies in package.json

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div>App</div>
+    // use Tailwind's `border` (1px) utility, not `border-1`
+    <div className='border border-red-200 bg-blue-200'>App</div>
   )
 }
 
